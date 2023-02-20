d112c6f3739fde7e0389a94cd7d4a044912e4230
baardl <bard.lind@gmail.com>
2023-02-20 17:54:34 +0100

    Create Influxdb and Grafana from docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # influxdb-grafana
 Setup of grafana and Influx
+
+## Influx 2.x
+https://jet.dev/blog/secure-influxdb-setup-with-docker/I
+
+## Influx 1.x
+Based on https://towardsdatascience.com/get-system-metrics-for-5-min-with-docker-telegraf-influxdb-and-grafana-97cfd957f0ac
+
+```
+server1$ docker run --rm \
+  -e INFLUXDB_DB=telegraf -e INFLUXDB_ADMIN_ENABLED=true \
+  -e INFLUXDB_ADMIN_USER=admin \
+  -e INFLUXDB_ADMIN_PASSWORD=supersecretpassword \
+  -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=secretpassword \
+  -v influxdb-volume:/var/lib/influxdb \
+  influxdb /init-influxdb.sh
+```
+
+Compose
+```
+docker-compose up -d
+```
+
+Verify influxdb 
+http://<publicip>:8086/
+Login using admin, and using influxdb usek
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# 1. Setup InfluxDb
+# 2. Create Grafana token
+# 3. Stop Influx
+# 4. Docker-compose with InfluDb and Grafana
+
+docker network create monitoring
+docker volume create grafana-volume
+docker volume create influxdb-data-volume
+docker volume create influxdb-config-volume
+
+docker run -d -p 8086:8086 \
+  --name influxdb \
+  -e DOCKER_INFLUXDB_INIT_MODE=setup \
+  -e DOCKER_INFLUXDB_INIT_USERNAME=root \
+  -e DOCKER_INFLUXDB_INIT_PASSWORD=secret-password \
+  -e DOCKER_INFLUXDB_INIT_ORG=my-init-org \
+  -e DOCKER_INFLUXDB_INIT_BUCKET=my-init-bucket \
+  -e DOCKER_INFLUXDB_INIT_ADMIN_TOKEN=secret-token \
+  -v influxdb-data-volume:/var/lib/influxdb \
+  -v influxdb-config-volume:/etc/influxdb \
+  influxdb:2.6
+
+
